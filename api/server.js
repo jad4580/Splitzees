578fdccdbd776d9be67183a10dbac2fcc109fec3
jad4580 <jdoaner98@yahoo.com>
2019-11-09 20:11:35 -0500

    blah

--- api/server.js
+++ api/server.js
@@ -2,9 +2,18 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://admin:admin@cluster-rp8uo.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, db) {
-    if(err) throw err;
-    db.collectionNames(function(err, collections){
-        console.log(collections);
-    });
+MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, (err, client) => {
+    if (err) {
+        console.error(err);
+        return
+    }
+    const db = client.db('test');
+    const collection = db.collection('authors');
+
+    collection.find().toArray((err, items) => {
+        console.log(items);
+    })
 });
